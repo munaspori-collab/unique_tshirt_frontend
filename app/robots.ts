77912966e42797c480f32e21de984f5ca4e20b65
Abir7109 <rahikulmakhtum147@gmail.com>
2025-11-09 02:46:11 +0600

    Add force-static config to manifest and robots for static export

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,7 @@
 import { MetadataRoute } from 'next';
 
+export const dynamic = 'force-static';
+
 export default function robots(): MetadataRoute.Robots {
   const baseUrl = 'https://munaspori-collab.github.io/unique_tshirt_frontend';
 
